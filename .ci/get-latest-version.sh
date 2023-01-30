b957e67ad44dc788ee49164bb8aef6b44772b7d7
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-01-30 15:03:14 +0100

    Gather Update.Version

--- .ci/get-latest-version.sh
+++ .ci/get-latest-version.sh
@@ -2,5 +2,5 @@
 ## This script prints the Version for go.elastic.co/apm/v2 in the stdout
 
 set -eo pipefail
-go list -json -u -m go.elastic.co/apm/v2 | jq -r .Version
+go list -json -u -m go.elastic.co/apm/v2 | jq -r .Update.Version
 exit $?
