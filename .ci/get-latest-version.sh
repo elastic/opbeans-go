aedaf82e026096440dd3b0fce119433577097d52
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-01-31 10:34:49 +0100

    unrequired

--- .ci/get-latest-version.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-## This script prints the Version for go.elastic.co/apm/v2 in the stdout
-
-set -eo pipefail
-
-go list -json -u -m go.elastic.co/apm/v2 | jq -r 'try (.Update.Version) // .Version'
-exit $?
