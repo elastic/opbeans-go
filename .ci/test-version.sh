345ab56f5794636c81e4dc98bc25187277025aa6
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-01-31 13:11:23 +0100

    refactor

--- .ci/test-version.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-## This script compares whether the given version defined in mod.go and the provided version
-## are equally or not.
-
-set -eo pipefail
-test $(go list -json -m go.elastic.co/apm/v2 | jq -r .Version) != $1
-exit $?
