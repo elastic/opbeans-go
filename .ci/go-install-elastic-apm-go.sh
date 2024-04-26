13bd1ea9c95ca27a07adf3643cea54c3ab8b8418
Victor Martinez <VictorMartinezRubio@gmail.com>
2024-04-26 10:24:21 +0200

    test-provenance

--- .ci/go-install-elastic-apm-go.sh
+++ .ci/go-install-elastic-apm-go.sh
@@ -4,4 +4,11 @@
 set -eo pipefail
 
 go get "go.elastic.co/apm/...@$1"
+
+# git diff is a safe choice because it will be non-empty when changes need to be committed and it's also good for
+# debugging.
+git diff
+
 go mod tidy
+
+git diff
